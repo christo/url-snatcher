6e26a9bfde3ebeec0dc16e8140de3dc5f88fd9cf
Chris Mountford <chris@think.net.au>
2020-11-12 00:41:04 +1100

    initial commitial

---
+++ url-snatcher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pyperclip
+import time
+
+# monitor paste buffer
+# if paste buffer contains a new url, append it to the urls file
+MIN_URL_LEN = 7
+
+filename="urls.txt" 
+# TODO take filename as arg
+# TODO read the last line out of the file
+prev_buff = ""
+while (True):
+    buff = pyperclip.paste()
+    if (len(buff) > MIN_URL_LEN and buff != prev_buff and buff.startswith("http")):
+        f= open(filename,"a+")
+        prev_buff = buff
+        f.write("%s\n" % (buff))
+        f.close()
+    time.sleep(0.4)
+
+
+
